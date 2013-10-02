837c2a9956a46fbf594a5a5c9ce60cbbb301e130
Matthias Klumpp <matthias@tenstral.net>
2013-10-01 22:31:27 -0700

    rebuilder: Prepare for automatic needs-rebuild package-finding

--- autorebuild/autorebuild.py
+++ autorebuild/autorebuild.py
@@ -22,6 +22,7 @@ import apt_pkg
 import subprocess
 import shutil
 from update_source import bump_source_version
+from apt_pkg import TagFile, TagSection
 from optparse import OptionParser
 from synctool.pkginfo import *
 from ConfigParser import SafeConfigParser
@@ -29,69 +30,111 @@ from ConfigParser import SafeConfigParser
 #REPO_POOL ="http://archive.tanglu.org/tanglu/pool"
 REPO_POOL = "file:///srv/dak/ftp/pool"
 
-def get_package_lists(suite):
-    parser = SafeConfigParser()
-    parser.read(['/srv/dak/sync-debian.conf', 'sync-debian.conf'])
-    _momArchivePath = parser.get('MOM', 'path')
-    _dest_distro = parser.get('SyncTarget', 'distro_name')
-    extra_suite = parser.get('SyncTarget', 'devel_suite')
-    if suite == extra_suite:
-        suite = "staging"
-    else:
-        extra_suite = ""
+class Autorebuild():
+    def __init__(suite):
+        parser = SafeConfigParser()
+        parser.read(['/srv/dak/sync-debian.conf', 'sync-debian.conf'])
+        self._ArchivePath = parser.get('Archive', 'path')
+        _dest_distro = parser.get('SyncTarget', 'distro_name')
+        extra_suite = parser.get('SyncTarget', 'devel_suite')
+        if suite == extra_suite:
+            suite = "staging"
+        else:
+            extra_suite = ""
 
-    pkginfo_tgl = PackageInfoRetriever(_momArchivePath, _dest_distro, suite)
-    pkginfo_tgl.extra_suite = extra_suite
-    # we only care about packages in main right now
-    pkgs_tanglu = pkginfo_tgl.get_packages_dict("main")
+        pkginfo_tgl = PackageInfoRetriever(self._ArchivePath, _dest_distro, suite)
+        pkginfo_tgl.extra_suite = extra_suite
+        # we only care about packages in main right now
+        self._pkgs_tanglu = pkginfo_tgl.get_packages_dict("main")
+        self._suite = suite
 
-    return pkgs_tanglu
+        # make sure workspace is empty...
+        if os.path.exists("/tmp/arb-workspace"):
+            shutil.rmtree("/tmp/arb-workspace")
 
-def download_pkg_to_workspace(workspace, suite, component, pkgname, version):
-    os.chdir(workspace)
+    def _download_pkg_to_workspace(self, workspace, suite, component, pkgname, version):
+        os.chdir(workspace)
 
-    if pkgname.startswith("lib"):
-        short_sec = pkgname[0:4]
-    else:
-        short_sec = pkgname[0:1]
-    url = "%s/%s/%s/%s/%s_%s.dsc" % (REPO_POOL, component, short_sec, pkgname, pkgname, noEpoch(version))
-    dget_cmd = ["dget", "-duq", url]
-    proc = subprocess.Popen(dget_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = proc.communicate()
-    output = ("%s\n%s") % (stdout, stderr)
-    if (proc.returncode != 0):
-        print(output)
-        return False
-    return True
+        if pkgname.startswith("lib"):
+            short_sec = pkgname[0:4]
+        else:
+            short_sec = pkgname[0:1]
+        url = "%s/%s/%s/%s/%s_%s.dsc" % (REPO_POOL, component, short_sec, pkgname, pkgname, noEpoch(version))
+        dget_cmd = ["dget", "-duq", url]
+        proc = subprocess.Popen(dget_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = proc.communicate()
+        output = ("%s\n%s") % (stdout, stderr)
+        if (proc.returncode != 0):
+            print(output)
+            return False
+        return True
 
-def trigger_package_rebuild(suite, component, pkgname, build_note):
-    workspace = os.path.abspath("/tmp/arb-workspace/%s" % (pkgname))
-    if not os.path.exists(workspace):
-        os.makedirs(workspace)
-    packages = get_package_lists(suite)
-    ret = download_pkg_to_workspace(workspace, suite, component, pkgname, packages[pkgname].version)
-    if not ret:
-        print("Download of package '%s' failed." % (pkgname))
-        return False
-    ret, dsc_path = bump_source_version(workspace, pkgname, build_note)
-    if not ret:
-        print("Unable to bump version of source package '%s'. It might need a manual upload." % (pkgname))
-        return False
+    def trigger_package_rebuild(self, component, pkgname, build_note):
+        workspace = os.path.abspath("/tmp/arb-workspace/%s" % (pkgname))
+        if not os.path.exists(workspace):
+            os.makedirs(workspace)
+        packages = self._pkgs_tanglu
+        ret = self._download_pkg_to_workspace(workspace, self._suite, component, pkgname, packages[pkgname].version)
+        if not ret:
+            print("Download of package '%s' failed." % (pkgname))
+            return False
+        ret, dsc_path = bump_source_version(workspace, pkgname, build_note)
+        if not ret:
+            print("Unable to bump version of source package '%s'. It might need a manual upload." % (pkgname))
+            return False
 
-    # now make the package known to dak
-    cmd = ["dak", "import", "-s", "-a", "staging", component, dsc_path]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    p.wait()
-    if p.returncode is not 0:
-        stdout, stderr = p.communicate()
-        print("ERR: %s\n%s %s" % (cmd, stdout, stderr))
-        raise Exception("Error while running dak!")
-        return False
+        # now make the package known to dak
+        cmd = ["dak", "import", "-s", "-a", "staging", component, dsc_path]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p.wait()
+        if p.returncode is not 0:
+            stdout, stderr = p.communicate()
+            print("ERR: %s\n%s %s" % (cmd, stdout, stderr))
+            raise Exception("Error while running dak!")
+            return False
 
-    shutil.rmtree(workspace)
-    print("Triggered rebuild for '%s'." % (pkgname))
+        shutil.rmtree(workspace)
+        print("Triggered rebuild for '%s'." % (pkgname))
 
-    return True
+        return True
+
+    def batch_rebuild_packages(self, component, bad_depends, build_note, dry_run=True):
+        source_path = self._ArchivePath + "%s/dists/%s/%s/binary-i386/Packages.gz" % ("tanglu", self._suite, component)
+        f = gzip.open(source_path, 'rb')
+        tagf = TagFile (f)
+        rebuildSources = []
+        for section in tagf:
+            pkgname = section['Package']
+            source_pkg = section.get('Source', '')
+            if source_pkg == '':
+                source_pkg = pkgname
+            if source_pkg in processedSources:
+                continue # we already handled a rebuild for that
+
+            depends = section.get('Depends', '')
+            # we ignore pre-depends: Pre-depending stuff is much safer with a manual rebuild
+            dep_chunks = depends.split(',')
+            for dep in dep_chunks:
+                dep = dep.strip()
+                if dep.startswith(bad_depends):
+                    if dep == bad_depends.strip():
+                        rebuildSources.append(source_pkg)
+                        continue
+                    if '(' not in dep:
+                        continue
+                    depid_parts = dep.split('(')
+                    if dep == depid_parts[0]:
+                        rebuildSources.append(source_pkg)
+                        continue
+
+            print("Packages planned for rebuild:")
+            if len(rebuildSources) == 0:
+                print("No matching packages found.")
+                return
+            print(rebuildSources.join('\n'))
+
+            if dry_run:
+                return
 
 def main():
     # init Apt, we need it later
@@ -119,10 +162,8 @@ def main():
             print("No build-note set! Please specify a rebuild reason (e.g. 'perl-5.18')")
             sys.exit(1)
 
-        # make sure workspace is empty...
-        if os.path.exists("/tmp/arb-workspace"):
-            shutil.rmtree("/tmp/arb-workspace")
-        ret = trigger_package_rebuild(suite, component, package_name, build_note)
+        rebuilder = Autorebuild(suite)
+        ret = rebuilder.trigger_package_rebuild(component, package_name, build_note)
         if not ret:
             sys.exit(2)
     else:
