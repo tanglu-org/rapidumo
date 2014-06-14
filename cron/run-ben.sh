f19a6cd621025f0a6cb5974d015978ba7c5a7d22
Matthias Klumpp <matthias@tenstral.net>
2014-06-14 12:45:22 +0200

    Manually invalidate Ben cache
    Should happen automatically on archive change, but for some reason
    doesn't happen.
    (Maybe because the projectb codepath isn't tested enough)

--- cron/run-ben.sh
+++ cron/run-ben.sh
@@ -4,6 +4,9 @@ set -e
 mkdir -p /srv/dak/tmp/ben
 cd /srv/dak/tmp/ben
 
+# remove the cache manually - cache invalodation doesn't seem to work
+rm -f ben.cache
+
 # just call ben to regenerate the transition tracker data
 ben tracker -g /srv/dak/archive-hints/ben/config/global.conf
 cd ..
