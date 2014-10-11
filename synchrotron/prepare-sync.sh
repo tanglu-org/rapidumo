6f5531a58a76ec7ff5a4d46c96c9898acbad70aa
Matthias Klumpp <matthias@tenstral.net>
2014-10-11 18:40:13 +0200

    Move sync blacklist

--- synchrotron/prepare-sync.sh
+++ synchrotron/prepare-sync.sh
@@ -24,7 +24,7 @@ BASE=/var/archive-kit/rapidumo
 # Update the blacklist
 sh $BASE/cron/sync-hints.sh
 rm /srv/patches.tanglu.org/sync-blacklist.txt
-cp /srv/dak/archive-hints/sync-blacklist/sync-blacklist.txt /srv/patches.tanglu.org/sync-blacklist.txt
+cp /srv/dak/archive-hints/synchrotron/sync-blacklist.txt /srv/patches.tanglu.org/sync-blacklist.txt
 
 cd $MOMDIR
 
